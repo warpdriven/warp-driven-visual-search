4d9329644c86de62788c7d44f1ad8f943e30b1d4
zhangfujiang <18604438176@163.com>
2023-05-16 20:27:23 +0800

    modify icon

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'warp-driven/wd-vs-woo',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e9f5b97fcc8db96160b31537f722e039cb7dfe03',
+        'reference' => 'b588d80505ff974905b2ead837e1c46b3bdc15ac',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'warp-driven/wd-vs-woo' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e9f5b97fcc8db96160b31537f722e039cb7dfe03',
+            'reference' => 'b588d80505ff974905b2ead837e1c46b3bdc15ac',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
