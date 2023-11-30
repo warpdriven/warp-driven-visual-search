6db81fd73cb5b1ef6fc843316dddbae218127f7d
ZhangSheng <yanglee2421@gmail.com>
2023-11-30 17:57:36 +0800

    Add form components

--- frontend/src/pages/settings/Settings.tsx
+++ frontend/src/pages/settings/Settings.tsx
@@ -2,12 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Components Imports
+import { SettingsForm } from "./SettingsForm";
+
 export function Settings() {
   const mainNode = React.useMemo(() => {
     const adminEl = document.getElementById("warpdriven-recs-admin");
     if (!adminEl) return null;
 
-    return ReactDOM.createPortal(<>Admin Settings</>, adminEl);
+    return ReactDOM.createPortal(<SettingsForm />, adminEl);
   }, []);
 
   return <>{mainNode}</>;
