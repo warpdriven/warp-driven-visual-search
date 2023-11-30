a71f84dae1295563ce7023fe4a467f4ef7c5b532
zhangfujiang <18604438176@163.com>
2023-11-30 22:05:31 +0800

    修改配置界面

--- devops/prod/package.sh
+++ devops/prod/package.sh
@@ -2,9 +2,6 @@ git pull
 
 composer upgrade
 
-npm install
-npm run build
-
 mkdir -p ./target/prod
 rm ./target/prod/*.zip
 zip -r  ./target/prod/plugin-warpdriven-vsr.zip . -x "devops/**" "node_modules/**" "target/**" ".*"
--- wd-vs-woo.php
+++ wd-vs-woo.php
@@ -31,7 +31,6 @@ if (!function_exists('wd_woo_plugin_vs_deactivation')) {
     function wd_woo_plugin_vs_deactivation()
     {
         delete_option('wd_api_key');
-        delete_option('wd_vs_init');
     }
 }
 
@@ -42,41 +41,4 @@ add_action('init', function () {
     if (!defined('WD_WPCore_Loaded')) {
         $core = new WPCore();
     }
-});
-
-add_action('admin_enqueue_scripts', function () {
-    $plugin_version = get_plugin_data(WD_WP_CORE_PLUGIN_FILE)['Version'];
-    wp_enqueue_style('wd-woo-plugin-vs-style', plugins_url('/assets/css/backend.css', WD_WP_CORE_PLUGIN_FILE), array(), $plugin_version);
-    wp_style_add_data('wd-woo-plugin-vs-style', 'rtl', 'replace');
-    wp_enqueue_script('wd-woo-plugin-vs-script', plugins_url('/assets/js/backend.js', WD_WP_CORE_PLUGIN_FILE), array('jquery'), $plugin_version, true);
-    wp_localize_script('wd-woo-plugin-vs-script', 'wd_woo_plugin_vs', array(
-        'ajax_url' => admin_url('admin-ajax.php'),
-        'wd_vs_init' => sanitize_text_field(get_option("wd_vs_init"))
-    ));
-});
-
-add_action('wp_enqueue_scripts', function () {
-    $plugin_version = get_plugin_data(WD_WP_CORE_PLUGIN_FILE)['Version'];
-    wp_enqueue_style('wd-woo-plugin-vs-frontend-style', plugins_url('/assets/css/frontend.css', WD_WP_CORE_PLUGIN_FILE), array(), $plugin_version);
-    wp_style_add_data('wd-woo-plugin-vs-frontend-style', 'rtl', 'replace');
-    wp_enqueue_script('wd-woo-plugin-vs-frontend-script', plugins_url('/assets/js/frontend.js', WD_WP_CORE_PLUGIN_FILE), array('jquery'), $plugin_version, true);
-    wp_localize_script('wd-woo-plugin-vs-frontend-script', 'wd_woo_plugin_vs', array(
-        'ajax_url' => admin_url('admin-ajax.php'),
-        'wd_search_product_enable'=>sanitize_text_field(get_option("wd_search_product_enable")),
-        'icon_css'=>array(
-            'size'=>sanitize_text_field(get_option("wd_search_product_icon_size")),
-            'top'=>sanitize_text_field(get_option("wd_search_product_icon_top")),
-            'left'=>sanitize_text_field(get_option("wd_search_product_icon_left")),
-            'right'=>sanitize_text_field(get_option("wd_search_product_icon_right")),
-            'bottom'=>sanitize_text_field(get_option("wd_search_product_icon_bottom"))
-        ),
-        'test_icon_css'=>array(
-            'size'=>sanitize_text_field(get_option("wd_search_product_test_icon_size")),
-            'top'=>sanitize_text_field(get_option("wd_search_product_test_icon_top")),
-            'left'=>sanitize_text_field(get_option("wd_search_product_test_icon_left")),
-            'right'=>sanitize_text_field(get_option("wd_search_product_test_icon_right")),
-            'bottom'=>sanitize_text_field(get_option("wd_search_product_test_icon_bottom"))
-        ),
-        'wd_vs_init' => sanitize_text_field(get_option("wd_vs_init"))
-    ));
 });
