3909668e40936ce4a48c3545284097ed46297690
ZhangSheng <yanglee2421@gmail.com>
2023-11-30 11:59:16 +0800

    Theme configuration

--- frontend/src/theme/theme-provider.tsx
+++ frontend/src/theme/theme-provider.tsx
@@ -1,12 +1,90 @@
 // MUI Imports
-import { ThemeProvider as MuiThemeProvider } from "@mui/material";
+import {
+  BreakpointsOptions,
+  createTheme,
+  GlobalStyles,
+  ScopedCssBaseline,
+  ThemeProvider as MuiThemeProvider,
+  alpha,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 // Theme Imports
-import { theme } from "./theme";
+import { shadowsMap } from "./shadowsMap";
 
 // React Imports
 import React from "react";
 
 export function ThemeProvider(props: React.PropsWithChildren) {
-  return <MuiThemeProvider theme={theme} {...props} />;
+  // ** Props
+  const { children } = props;
+
+  const mode = "light";
+  const whiteColor = "#fff";
+
+  const theme = createTheme({
+    breakpoints: breakpoints(),
+    spacing(abs: number) {
+      return `${abs * 0.25}rem`;
+    },
+    shape: {
+      borderRadius: 6,
+    },
+
+    shadows: shadowsMap.get(mode),
+
+    palette: {
+      mode: "light",
+      common: {
+        black: "#000",
+        white: whiteColor,
+      },
+      grey: {
+        50: "#FAFAFA",
+        100: "#F5F5F5",
+        200: "#EEEEEE",
+        300: "#E0E0E0",
+        400: "#BDBDBD",
+        500: "#9E9E9E",
+        600: "#757575",
+        700: "#616161",
+        800: "#424242",
+        900: "#212121",
+        A100: "#F5F5F5",
+        A200: "#EEEEEE",
+        A400: "#BDBDBD",
+        A700: "#616161",
+      },
+
+      secondary: {
+        main: grey[500],
+        light: grey[400],
+        dark: grey[600],
+        contrastText: whiteColor,
+      },
+
+      divider: alpha(grey[900], 0.12),
+    },
+  });
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <GlobalStyles styles={{}} />
+      <ScopedCssBaseline>{children}</ScopedCssBaseline>
+    </MuiThemeProvider>
+  );
+}
+
+function breakpoints(): BreakpointsOptions {
+  return {
+    keys: ["xs", "sm", "md", "lg", "xl"],
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+    unit: "px",
+  };
 }
