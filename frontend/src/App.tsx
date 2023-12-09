50b75712cd6a04432552cf976b5d36863cb91479
ZhangSheng <yanglee2421@gmail.com>
2023-12-09 11:59:42 +0800

    Remove redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 // Provider Imports
 import { QueryProvider } from "@/api/provider";
 import { ThemeProvider } from "@/theme";
-import { ReduxProvider } from "@/redux";
 
 // Toast Imports
 import { Toaster } from "react-hot-toast";
@@ -11,13 +10,11 @@ import { RootRoute } from "@/router";
 
 export function App() {
   return (
-    <ReduxProvider>
-      <QueryProvider>
-        <Toaster position="bottom-left" />
-        <ThemeProvider>
-          <RootRoute />
-        </ThemeProvider>
-      </QueryProvider>
-    </ReduxProvider>
+    <QueryProvider>
+      <Toaster position="bottom-left" />
+      <ThemeProvider>
+        <RootRoute />
+      </ThemeProvider>
+    </QueryProvider>
   );
 }
