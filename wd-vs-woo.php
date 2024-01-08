1ec6a417cf97002ef5baddc278a12cb548b75499
ZhangSheng <yanglee2421@gmail.com>
2024-01-08 16:17:09 +0800

    build: v0.0.7

--- wd-vs-woo.php
+++ wd-vs-woo.php
@@ -7,7 +7,7 @@ Author: Warp Driven Technology
 Author URI: https://warp-driven.com/
 Text Domain: wd-wgc-woo
 Domain Path: /languages/
-Version: 0.0.6
+Version: 0.0.7
 */
 
 if (!defined('ABSPATH')) {
